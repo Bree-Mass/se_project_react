d9860078db99cb69f26dd19acad6f66781269c66
Bree Massingill <breemassingill@gmail.com>
2024-07-25 13:08:24 -0500

    create form modal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -17,7 +17,68 @@ const App = () => {
         <Footer />
       </div>
       {/* {activeModal === "create" && ( */}
-      <ModalWithForm title="new garment" name="new-card" />
+      <ModalWithForm titleText="New garment" buttonText="Add garment">
+        <label className="modal__label" htmlFor="name">
+          Name
+          <input
+            className="modal__input modal__input_card-name"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            minLength="1"
+            maxLength="30"
+            required
+          />
+          <span className="modal__error"></span>
+        </label>
+        <label className="modal__label" htmlFor="imageUrl">
+          Image
+          <input
+            className="modal__input  modal__input_url"
+            type="url"
+            name="link"
+            id="imageUrl"
+            placeholder="Image URL"
+            required
+          />
+          <span className="modal__error"></span>
+        </label>
+
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label className="modal__label_radio" htmlFor="choiceHot">
+            <input
+              className="modal__input_radio"
+              type="radio"
+              id="choiceHot"
+              name="weatherType"
+              value="hot"
+            />
+            Hot
+          </label>
+          <label className="modal__label_radio" htmlFor="choiceWarm">
+            <input
+              className="modal__input_radio"
+              type="radio"
+              id="choiceWarm"
+              name="weatherType"
+              value="warm"
+            />{" "}
+            Warm
+          </label>
+          <label className="modal__label_radio" htmlFor="choiceCold">
+            <input
+              className="modal__input_radio"
+              type="radio"
+              id="choiceCold"
+              name="weatherType"
+              value="cold"
+            />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
       {/* )} */}
       {/* {activeModal === "preview" && (
         <ItemModal card={selectedCard} onClose={closeAllModals} />
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,69 +1,20 @@
 import "./modalWithForm.css";
 
-function ModalWithForm() {
+function ModalWithForm(props) {
   return (
     <div className="modal">
-      <form action="" className="modal__form">
-        <h2 className="modal__title">New garment</h2>
-        <button type="button" className="modal__close">
+      <div className="modal__content">
+        <h2 className="modal__title">{props.titleText}</h2>
+        <button className="modal__close-button" type="button">
           CLOSE
         </button>
-        <label className="modal__label" htmlFor="name">
-          <input
-            className="modal__input modal__input_card-name"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            minLength="1"
-            maxLength="30"
-            required
-          />
-          <span className="modal__error"></span>
-        </label>
-        <label className="modal__label" htmlFor="imageUrl">
-          <input
-            className="modal__input  modal__input_url"
-            type="url"
-            name="link"
-            id="imageUrl"
-            placeholder="Image URL"
-            required
-          />
-          <span className="modal__error"></span>
-        </label>
-        <p>Select the weather type:</p>
-        <div className="modal__input  modal__input_radio">
-          <div>
-            <input type="radio" id="choiceHot" name="weatherType" value="hot" />
-            <label className="modal__label_radio" htmlFor="choiceHot">
-              Hot
-            </label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="choiceWarm"
-              name="weatherType"
-              value="warm"
-            />
-            <label className="modal__label_radio" htmlFor="choiceWarm">
-              Warm
-            </label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="choiceCold"
-              name="weatherType"
-              value="cold"
-            />
-            <label className="modal__label_radio" htmlFor="choiceCold">
-              Cold
-            </label>
-          </div>
-        </div>
-      </form>
+        <form action="" className="modal__form">
+          {props.children}
+          <button className="modal__submit-button" type="submit">
+            {props.buttonText}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
